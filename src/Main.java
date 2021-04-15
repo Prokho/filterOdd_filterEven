6549da4bc72a1c7258aafe498d2ee77f5e4f5ce6
Prokhor Proshkin <pproshkin@yandex.ru>
2021-04-15 21:06:28 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,107 @@
+//ДЗ написать функцию генерации массива заполненого случайными числами от 0 до 100
+//2 дз написать функцию фильтрации четных элементов массива
+//по итогу функция должна создать 2 массив на базе входящего массива
+//2 массив должен содержать только четные элементы
+
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class Main {
+
+  static int [] generateRandomeArray(int size)
+  {
+      int [] array = new int[size];
+      Random rnd = new Random();
+      for(int i =0; i < array.length; i++)
+      {
+          array[i] = rnd.nextInt(100);
+      }
+      return array;
+  }
+
+  static int [] filterEven(int [] array)
+  {
+      int counter =0;
+      for(int i=0;i< array.length;i++)
+      {
+          if(array[i]%2==0)
+          {
+              counter=counter+1;
+          }
+
+      }
+      if(counter==0){
+          return null;
+      }
+      int [] arrayEven = new int[counter];
+
+      for(int i=0, j=0;i< array.length;i++)
+      {
+          if(array[i]%2==0)
+          {
+              arrayEven[j] = array[i];
+              j +=1;
+          }
+
+      }
+      return arrayEven;
+
+  }
+
+    static int [] filterOdd(int [] array)
+    {
+        int counter =0;
+        for(int i=0;i< array.length;i++)
+        {
+            if(array[i]%2==1)
+            {
+                counter=counter+1;
+            }
+
+        }
+        if(counter==0){
+            return null;
+        }
+        int [] arrayEven = new int[counter];
+
+        for(int i=0, j=0;i< array.length;i++)
+        {
+            if(array[i]%2==1)
+            {
+                arrayEven[j] = array[i];
+                j +=1;
+            }
+
+        }
+        return arrayEven;
+
+    }
+
+  static void print(int [] array)
+  {
+      for(int i=0; i< array.length; i++)
+      {
+          System.out.print(array[i] + " ");
+      }
+      System.out.println();
+  }
+
+    public static void main(String[] args) {
+        int [] array1 = generateRandomeArray(25);
+        int [] array2 = generateRandomeArray(50);
+        print(array1);
+        print(array2);
+        int [] filteredArray1 = filterEven(array1);
+        int [] filteredArray2 = filterEven(array2);
+        print(filteredArray1);
+        print(filteredArray2);
+        int [] filteredArray3 = filterOdd(array1);
+        int [] filteredArray4 = filterOdd(array2);
+        print(filteredArray3);
+        print(filteredArray4);
+
+    }
+
+}
